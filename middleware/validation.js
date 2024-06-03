3a1ee8d16f169e0c34546dc0a22778aaacd3ab07
dgschooldevelopment <dreamsguider@gmail.com>
2024-06-03 12:19:16 +0530

    Update validation.js

--- middleware/validation.js
+++ middleware/validation.js
@@ -3,7 +3,7 @@ module.exports = {
       console.log('Request Body:', req.body); // Log the request body for debugging
   
       if (!req.body || !req.body.college_code ) {
-        return res.status(400).json({ error: 'collegeCode is missing in the request body' });
+        return res.status(400).json({ error: 'college_code is missing in the request body' });
       }
       next();
     }
