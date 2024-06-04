e1b425295308a54136467573674e6ecc86f8800a
dgschooldevelopment <dreamsguider@gmail.com>
2024-06-04 12:33:36 +0530

    Update collegeController.js

--- controllers/collegeController.js
+++ controllers/collegeController.js
@@ -1,3 +1,4 @@
+/*
 const { collegesPool } = require('../config/dbconfig');
 
 const checkCollege = async (req, res) => {
@@ -35,4 +36,34 @@ const checkCollege = async (req, res) => {
 
 module.exports = {
     checkCollege
+};*/
+const { collegesPool } = require('../config/dbconfig');
+
+const checkCollege = async (req, res) => {
+    const { college_code  } = req.body;
+
+    if (!college_code ) {
+        return res.status(400).json({ error: 'collegeCode is a required parameter' });
+    }
+
+    const sql = `SELECT * FROM College WHERE college_code = ?`;
+
+    try {
+        const [results] = await collegesPool.query(sql, [college_code ]);
+
+        if (results.length === 0) {
+            return res.status(404).json({ error: 'College code not found' });
+        }
+
+        const college = results[0];
+        return res.status(200).json({ success: true, message: 'College code found', college });
+    } catch (error) {
+        console.error('Error executing query:', error);
+        return res.status(500).json({ error: 'Internal server error' });
+    }
 };
+
+module.exports = {
+    checkCollege
+};
+
