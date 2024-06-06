60fb7b92b71c16a6922bdfa73cba7d226efd5a2a
PratikshaPawar7350 <107267479+PratikshaPawar7350@users.noreply.github.com>
2024-06-06 11:08:12 +0530

    report api

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const express = require('express');
+/*const express = require('express');
 const dotenv = require('dotenv');
 const routes = require('./routes/routes');
 const cors = require('cors'); // Importing the cors package
@@ -20,6 +20,25 @@ app.use(bodyParser.json({ limit: '300mb' })); // Increase limit for JSON request
 app.use(bodyParser.urlencoded({ limit: '300mb', extended: true }));
 >>>>>>> b718d5b17207077832f053fc6d902cec4a174425
  // Ensure express can parse JSON bodies
+app.use('/', routes);
+
+const port = process.env.PORT || 3001;
+app.listen(port, () => console.log(`Server is running on port ${port}`));
+*/
+const express = require('express');
+const dotenv = require('dotenv');
+const routes = require('./routes/routes');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+
+const app = express();
+dotenv.config();
+
+app.use(cors());
+app.use(express.json({ limit: '300mb' }));
+app.use(bodyParser.json({ limit: '300mb' }));
+app.use(bodyParser.urlencoded({ limit: '300mb', extended: true }));
+
 app.use('/', routes);
 
 const port = process.env.PORT || 3001;
