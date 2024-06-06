ede9db1feee4e98c393e0de5fc34e984df613ec8
PratikshaPawar7350 <107267479+PratikshaPawar7350@users.noreply.github.com>
2024-06-06 11:11:34 +0530

    report api

--- controllers/collegeController.js
+++ controllers/collegeController.js
@@ -45,7 +45,7 @@ const checkCollege = async (req, res) => {
 module.exports = {
     checkCollege
 };*/
-const { collegesPool } = require('../config/dbconfig');
+/*const { collegesPool } = require('../config/dbconfig');
 
 const checkCollege = async (req, res) => {
     const { college_code  } = req.body;
@@ -59,6 +59,39 @@ const checkCollege = async (req, res) => {
     try {
 >>>>>>> b718d5b17207077832f053fc6d902cec4a174425
         const [results] = await collegesPool.query(sql, [college_code ]);
+
+        if (results.length === 0) {
+            return res.status(404).json({ error: 'College code not found' });
+        }
+
+        const college = results[0];
+        return res.status(200).json({ success: true, message: 'College code found', college });
+    } catch (error) {
+        console.error('Error executing query:', error);
+        return res.status(500).json({ error: 'Internal server error' });
+    }
+};
+
+module.exports = {
+    checkCollege
+};
+*/
+
+
+
+const { collegesPool } = require('../config/dbconfig');
+
+const checkCollege = async (req, res) => {
+    const { college_code } = req.body;
+
+    if (!college_code) {
+        return res.status(400).json({ error: 'collegeCode is a required parameter' });
+    }
+
+    const sql = `SELECT * FROM College WHERE college_code = ?`;
+
+    try {
+        const [results] = await collegesPool.query(sql, [college_code]);
 
         if (results.length === 0) {
             return res.status(404).json({ error: 'College code not found' });
