274a797c687e31f7211a5b053cd42d98a54ca145
dgschooldevelopment <dreamsguider@gmail.com>
2024-06-10 12:36:02 +0530

    Update routes.js

--- routes/routes.js
+++ routes/routes.js
@@ -114,18 +114,19 @@ const { unittest } = require('../controllers/unittest');
 const { report } = require('../controllers/report');
 const { submitHomework } = require('../controllers/submithomework');
 const { Assignment } = require('../controllers/Assignment'); // Ensure Assignment controller is properly defined
-const { Attendance } = require('../controllers/attendance'); // Ensure Assignment controller is properly defined
-const { insertAttendance, fetchStudentAttendance } = require('../controllers/insertfetchAttendance'); 
-const { addReason } = require('../controllers/addreason');
+const { Attendance } = require('../controllers/attendence'); // Ensure Assignment controller is properly defined
+const {insertAttendance,fetchStudentAttendance } = require('../controllers/insertfetchAttendance'); 
+
 const { teacherLogin } = require('../controllers/teacherLogin');
 const { teacherDashboard } = require('../controllers/teacher_dashboard');
 const { teacherProfile } = require('../controllers/teacherProfile');
 const { teacher_classList } = require('../controllers/teacher_classList');
 const { chapterPoints } = require('../controllers/chapter_points');
-const { DailyUpdates, getTeacherDailyUpdate } = require('../controllers/teacher_dailyupdate');
+const { DailyUpdates,  getTeacherDailyUpdate } = require('../controllers/teacher_dailyupdate');
 const { studentList } = require('../controllers/student_list');
 
 // Endpoint to check the college code
+const { addReason } = require('../controllers/addreason');
 router.post('/check', checkCollege);
 
 // Endpoint for student login
@@ -169,23 +170,24 @@ router.get('/report', validateCollegeCode, setupDatabaseConnection, report, clos
 
 // Endpoint for assignment
 router.get('/assignment', validateCollegeCode, setupDatabaseConnection, Assignment, closeDatabaseConnection);
-
-// Endpoint for attendance
+//endpoint for attendence
 router.get('/attendance', validateCollegeCode, setupDatabaseConnection, Attendance, closeDatabaseConnection);
 
-router.post('/insert', validateCollegeCode, setupDatabaseConnection, insertAttendance, closeDatabaseConnection);
+router.post('/insert',validateCollegeCode, setupDatabaseConnection, insertAttendance, closeDatabaseConnection);
 router.get('/fetchattendance', validateCollegeCode, setupDatabaseConnection, fetchStudentAttendance, closeDatabaseConnection);
-router.post('/add-reason', validateCollegeCode, setupDatabaseConnection, addReason, closeDatabaseConnection);
+router.post('/add-reason', validateCollegeCode, setupDatabaseConnection,addReason,closeDatabaseConnection);
 
-// Teacher API Endpoints
+// teacherAPI Endpoints
 
-router.post('/loginteacher', validateCollegeCode, setupDatabaseConnection, teacherLogin, closeDatabaseConnection);
+router.post('/loginteacher', validateCollegeCode, setupDatabaseConnection, teacherLogin,closeDatabaseConnection );
 router.get('/teacher_dashboard', teacherDashboard);
-router.get('/teacher_profile', validateCollegeCode, setupDatabaseConnection, teacherProfile, closeDatabaseConnection);
-router.get('/teacher_classlist', validateCollegeCode, setupDatabaseConnection, teacher_classList, closeDatabaseConnection);
-router.get('/chapter_points', chapterPoints);
-router.post('/teacher_dailyupdate', validateCollegeCode, setupDatabaseConnection, DailyUpdates, closeDatabaseConnection);
-router.get('/get_teacher_dailyupdate', validateCollegeCode, setupDatabaseConnection, getTeacherDailyUpdate, closeDatabaseConnection);
-router.get('/students', validateCollegeCode, setupDatabaseConnection, studentList, closeDatabaseConnection);
+router.get('/teacher_profile', validateCollegeCode, setupDatabaseConnection, teacherProfile, closeDatabaseConnection );
+router.get('/teacher_classlist', validateCollegeCode, setupDatabaseConnection, teacher_classList, closeDatabaseConnection );
+router.get('/chapter_points',chapterPoints);
+router.post('/teacher_dailyupdate', validateCollegeCode, setupDatabaseConnection, DailyUpdates,closeDatabaseConnection  );  
+router.get('/get_teacher_dailyupdate',validateCollegeCode, setupDatabaseConnection, getTeacherDailyUpdate,closeDatabaseConnection );
+router.get('/students', validateCollegeCode, setupDatabaseConnection, studentList,closeDatabaseConnection );
+
+
 
 module.exports = router;
